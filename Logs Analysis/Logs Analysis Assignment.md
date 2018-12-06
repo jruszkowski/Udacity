a7f709c4dec16b00a21fcd1b707a017dbf46fccb
jruszkowski <joseph.ruszkowski@gmail.com>
2018-12-06 14:50:01 +0000

    Logs Analysis Assignment

---
+++ Logs Analysis/Logs Analysis Assignment.md
@@ -0,0 +1,18 @@
+# Logs Analysis Assignment
+
+This is a reporting tool that prints out reports (in plain text) based on the data in the database. This reporting tool is a Python program using the psycopg2 module to connect to the database.
+
+The tool answers 3 questions:
+
+1. What are the most popular three articles of all time? Which articles have been accessed the most? 
+2. Who are the most popular article authors of all time? That is, when you sum up all of the articles each author has written, which authors get the most page views?
+3. On which days did more than 1% of requests lead to errors?
+
+Run
+----
+python3 program.py > project_output.txt
+ 
+License
+----
+
+MIT
---
+++ Logs Analysis/project.py
@@ -0,0 +1,60 @@
+import psycopg2
+
+DBNAME = "news"
+
+question_1 = "\
+        select b.title, count(a.path) as downloads \
+        from log a, articles b \
+        where substring(a.path,10,255) = b.slug \
+        and a.path!='/' \
+        and a.status = '200 OK' \
+        group by 1 \
+        order by 2 \
+        desc limit 3"
+
+question_2 = "\
+        select c.name, count(a.path) as downloads \
+        from log a, articles b, authors c \
+        where substring(a.path,10,255) = b.slug \
+                and b.author = c.id \
+        and a.path!='/' \
+        and a.status = '200 OK' \
+        group by 1 \
+        order by 2 \
+        desc limit 3"
+
+question_3 = "\
+        select \
+        a.date_notime,\
+        b.bad::int::float / a.total::int::float * 100 as errors\
+        from\
+        (select time::timestamp::date as date_notime,count(*) as total from log group by 1) a,\
+        (select time::timestamp::date as date_notime,count(*) as bad from log where status = '404 NOT FOUND' group by 1) b\
+        where a.date_notime = b.date_notime\
+        and (b.bad::int::float / a.total::int::float) > .01\
+        "
+
+def results(q):
+    db = psycopg2.connect(database=DBNAME)
+    c = db.cursor()
+    c.execute(q) 
+    posts = c.fetchall()
+    db.close()
+    return posts
+
+def pprint(l,t=' views'):
+    if t=='% errors':
+        for x in l:
+            print('%s, %.2f%s' % (x[0], x[1], t))
+    else:
+        for x in l:
+            print('%s, %d%s' % (x[0], x[1], t))
+
+
+if __name__ == '__main__': 
+    print('Question #1')
+    pprint(results(question_1))
+    print('Question #2')
+    pprint(results(question_2))
+    print('Question #3')
+    pprint(results(question_3), '% errors')
